edaa5934d4925a260ac62bf0f2d4e1013f75332c
1559830979 <1559830979@qq.com>
2019-11-18 02:51:03 +0800

    ss

---
+++ environmental.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : environmental
+Source Server Version : 80018
+Source Host           : localhost:3306
+Source Database       : 
+
+Target Server Type    : MYSQL
+Target Server Version : 80018
+File Encoding         : 65001
+
+Date: 2019-11-18 02:43:21
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
